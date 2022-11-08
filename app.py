cab9eda1eadc43cf40d90a725aa2ba031fd46043
Dhoni Hanif Supriyadi <dhonihanif354@gmail.com>
2022-11-08 20:27:00 +0700

    new

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session, url_for
 from flask_mysqldb import MySQL
 app = Flask(__name__)
 
@@ -6,35 +6,41 @@ app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = ""
 app.config["MYSQL_DB"] = "kursus"
+app.secret_key='asdsdfsdfs13sdf_df%&'
 
 mysql = MySQL(app)
 @app.route("/")
-def hello():
-    return render_template("index.html")
-
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
-@app.route("/home", methods=["POST"])
-def login_post():
-    username = request.form.get("name")
-    password = request.form.get("password")
+def index():
+    global curfet
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM login")
     curfet = cur.fetchall()
     user = [i[0] for i in curfet]
-    pw = [i[1] for i in curfet]
-    if username in user and password in pw:
-        return render_template("index.html")
+    login = False
+    if "username" in session:
+        login = True
+    return render_template("index.html", login=login, user=user)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("name")
+        password = request.form.get("password")
+        user = [i[0] for i in curfet]
+        pw = [i[1] for i in curfet]
+        if username in user and password in pw:
+            session["username"] = username
+            session["password"] = password
+            return redirect(url_for("index"))
+        else:
+            return render_template("login.html")
+
     else:
         return render_template("login.html")
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
-@app.route("/register_post", methods=["POST"])
-def register_post():
     if request.method == "POST":
         username = request.form.get("name")
         password = request.form.get("password")
@@ -43,23 +49,42 @@ def register_post():
         cur2 = cur.fetchall()
         mysql.connection.commit()
         cur.close()
-        return render_template("login.html")
-    
+        return redirect(url_for("register"))
+    else:
+        return render_template("register.html")
+
+@app.route("/logout")
+def logout():
+    session.pop('username',None)
+    return redirect(url_for('index'))
+
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    login = False
+    if "username" in session:
+        login = True
+    return render_template("about.html", login=login)
 
 @app.route("/kursus")
 def service():
-    return render_template("service.html")
+    login = False
+    if "username" in session:
+        login = True
+    return render_template("service.html", login=login)
 
 @app.route("/menu")
 def menu():
-    return render_template("menu.html")
+    login = False
+    if "username" in session:
+        login = True
+    return render_template("menu.html", login=login)
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    login = False
+    if "username" in session:
+        login = True
+    return render_template("contact.html", login=login)
 
 if __name__ == "__main__":
     app.run(debug=True)
