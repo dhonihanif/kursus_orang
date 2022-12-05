dc601eb7ff392c5d95e2386b73e19e16e3e91fc0
Dhoni Hanif Supriyadi <66652598+dhonihanif@users.noreply.github.com>
2022-12-06 00:07:14 +0700

    Add files via upload

--- kursus.sql
+++ kursus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 04, 2022 at 05:16 PM
+-- Generation Time: Dec 05, 2022 at 06:07 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
@@ -30,15 +30,20 @@ SET time_zone = "+00:00";
 CREATE TABLE `login` (
   `username` varchar(50) NOT NULL,
   `password` varchar(50) DEFAULT NULL,
-  `name` varchar(50) DEFAULT NULL
+  `name` varchar(50) DEFAULT NULL,
+  `usia` int(5) DEFAULT NULL,
+  `tempat_tinggal` varchar(50) DEFAULT NULL,
+  `telp` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `login`
 --
 
-INSERT INTO `login` (`username`, `password`, `name`) VALUES
-('dhonihanif', 'twinet', 'Dhoni Hanif Supriyadi');
+INSERT INTO `login` (`username`, `password`, `name`, `usia`, `tempat_tinggal`, `telp`) VALUES
+('admin', 'admin', 'Admin', 22, 'Jakarta', '08123456789'),
+('dhoni', 'twinet', 'Dhoni Hanif Supriyadi', 21, 'Jakarta', '0895326168335'),
+('dhonihanif', 'twinet', 'Dhoni Hanif Supriyadi', 21, 'Jakarta', '0895326168335');
 
 -- --------------------------------------------------------
 
@@ -53,6 +58,13 @@ CREATE TABLE `pesanan` (
   `pembayaran` varchar(10) DEFAULT NULL,
   `bayar` varchar(50) NOT NULL DEFAULT 'Belum bayar'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pesanan`
+--
+
+INSERT INTO `pesanan` (`pesan`, `username`, `name`, `pembayaran`, `bayar`) VALUES
+('Paket 1 Hari', 'dhonihanif', 'Dhoni Hanif Supriyadi', 'BRI', 'Sudah bayar');
 
 --
 -- Indexes for dumped tables
